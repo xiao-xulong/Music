c4ccc8ae0cfee3f1adf5566102bfeef2c02b1a33
snow <1529836290@qq.com>
2021-06-24 12:40:51 +0800

    '首页'

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Home from '../views/topbar'
+import Recommended from '../views/recommended'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    redirect: '/Recommended',
+    children:[
+      {
+        path: '/Recommended',
+        name: 'Rcommended',
+        component: Recommended,
+      }
+    ]
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+
 ]
 
 const router = new VueRouter({
---
+++ src/views/recommended.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      5555
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'recommended'
+  }
+</script>
+
+<style scoped>
+
+</style>
